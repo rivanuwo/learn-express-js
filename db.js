f7aaf1feca43cc05e3191cc39d6a3831e9266331
Rivan Nurdin <rivannurdin001@gmail.com>
2020-12-24 10:36:34 +0700

    bangsat

---
+++ db.js
@@ -0,0 +1,10 @@
+var sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
+
+sequelize
+  .authenticate()
+  .then(function(err) {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(function (err) {
+    console.log('Unable to connect to the database:', err);
+  });
