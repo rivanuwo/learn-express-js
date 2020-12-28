56c10440f9ee57efcd9c93bfd3fcc4897dc1c5fe
Rivan Nurdin <rivannurdin001@gmail.com>
2020-12-28 15:36:32 +0700

    update

--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,9 @@
-var   express = require('express');
-var   router  = express.Router();
-const model   = require('../models/index');
+var express = require('express');
+var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  try {
-    const users = await model.users.findAll({});
-    if (users.length !== 0) {
-      res.json({
-        'status': 'OK',
-        'messages': '',
-        'data': users
-      })
-    } else {
-      res.json({
-        'status': 'ERROR',
-        'messages': 'EMPTY',
-        'data': {}
-      })
-    }
-  } catch (err) {
-    res.json({
-      'status': 'ERROR',
-      'messages': err.message,
-      'data': {}
-    })
-  }
+  res.send('respond with a resource');
 });
 
 module.exports = router;
