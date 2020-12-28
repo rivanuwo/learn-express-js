a72cf00b32d8e6fe0716a237db0ccdb70d4f1413
Rivan Nurdin <rivannurdin001@gmail.com>
2020-12-28 15:22:48 +0700

    nodemon

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+var   express = require('express');
+var   router  = express.Router();
+const model   = require('../models/index');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  try {
+    const users = await model.users.findAll({});
+    if (users.length !== 0) {
+      res.json({
+        'status': 'OK',
+        'messages': '',
+        'data': users
+      })
+    } else {
+      res.json({
+        'status': 'ERROR',
+        'messages': 'EMPTY',
+        'data': {}
+      })
+    }
+  } catch (err) {
+    res.json({
+      'status': 'ERROR',
+      'messages': err.message,
+      'data': {}
+    })
+  }
 });
 
 module.exports = router;
